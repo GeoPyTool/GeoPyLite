fe3795141d65e2650687d68f062b2aadd4ccbb94
Frederick <softcheck@outlook.com>
2023-12-21 16:36:50 +0800

    update

---
+++ test.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageFilter
+import os
+
+# Get the current directory
+current_directory = os.getcwd()
+
+# Change the current directory to the file's directory
+file_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_directory)
+
+# Load the image
+image = Image.open("Birds.JPG")
+
+# Enhance the image quality
+enhanced_image = image.filter(ImageFilter.SHARPEN)
+
+# Increase the resolution
+new_size = (image.size[0] * 2, image.size[1] * 2)
+resized_image = enhanced_image.resize(new_size)
+
+# Save the enhanced and resized image
+resized_image.save("enhanced_Birds.JPG")
+
+# Change the current directory back to the original directory
+os.chdir(current_directory)
+
+
