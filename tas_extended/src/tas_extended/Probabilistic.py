3a485c80c1b672c82c55f3002cd0d9f5207ea0b9
Frederick <softcheck@outlook.com>
2024-01-26 09:03:10 +0800

    update

--- tas_extended/src/tas_extended/Probabilistic.py
+++ tas_extended/src/tas_extended/Probabilistic.py
@@ -8,13 +8,14 @@ import matplotlib.pyplot as plt
 import os
 
 
-def Compare(df_train= pd.DataFrame(),df_test= pd.DataFrame(), x='x',y='x'):
+def Compare(df_train= pd.DataFrame(),df_test= pd.DataFrame(), x_label='x',y_label='x'):
     # Extract x_train and y_train from df_train
-    x_train = df_train[x].values
-    y_train = df_train[y].values
+    x_train = df_train[x_label].values
+    y_train = df_train[y_label].values
 
     # Combine x_train and y_train into a 2D array
     data_train = np.column_stack((x_train, y_train))
+    
 
     # Use KMeans to find the cluster center
     kmeans = KMeans(n_clusters=1).fit(data_train)
@@ -30,15 +31,23 @@ def Compare(df_train= pd.DataFrame(),df_test= pd.DataFrame(), x='x',y='x'):
     # kde = KernelDensity(kernel='exponential').fit(data_train)
     # kde = KernelDensity(kernel='linear').fit(data_train)
 
+
+    n = 1024
+    x = np.linspace(35, 90, n)
+    y = np.linspace(0, 20, n)
+    data_whole = np.column_stack((x, y))
+    
     # Calculate the log probability density for each point in the training set
-    log_prob_density = kde.score_samples(data_train)
+    log_prob_density = kde.score_samples(data_whole)
 
     # Convert the log probability density to the original probability density
     prob_density = np.exp(log_prob_density)
 
+    max_prob_density = np.max( prob_density)
+
     # Extract x_test and y_test from df_test
-    x_test = df_test[x].values
-    y_test = df_test[y].values
+    x_test = df_test[x_label].values
+    y_test = df_test[y_label].values
 
     # Calculate the probability for each test point
     test_probabilities = []
@@ -51,7 +60,7 @@ def Compare(df_train= pd.DataFrame(),df_test= pd.DataFrame(), x='x',y='x'):
         test_probabilities = np.exp(test_densities)
 
         # Normalize probabilities
-        test_probabilities /= np.max(test_probabilities)
+        test_probabilities /= np.max( max_prob_density)
 
         # # Print probabilities
         # for i, prob in enumerate(test_probabilities):
@@ -87,28 +96,28 @@ x_test = np.array([45.76428218, 31.81390091, 55.28563359, 60.96200656, 70.437111
 y_test = np.array([13.76185634, 10.33199649, 8.79003757, 14.88086825, 9.92049699,
        2.49193062])
 
-x_test = np.linspace(40, 70, 2*n)
-y_test = np.linspace(2, 12, 6*n)
+x_test = np.linspace(40, 70, n)
+y_test = np.linspace(2, 12, n)
 
-x_test_mesh, y_test_mesh = np.meshgrid(x_test, y_test)
-test_points = np.c_[x_test_mesh.ravel(), y_test_mesh.ravel()]
-df_test = pd.DataFrame(test_points, columns=['SIO2_wt_calibred', 'ALL_Alkaline_wt_calibred'])
-# df_test = pd.DataFrame({'x': x_test, 'y': y_test})
+# x_test_mesh, y_test_mesh = np.meshgrid(x_test, y_test)
+# test_points = np.c_[x_test_mesh.ravel(), y_test_mesh.ravel()]
+# df_test = pd.DataFrame(test_points, columns=['SIO2_wt_calibred', 'ALL_Alkaline_wt_calibred'])
+df_test = pd.DataFrame({'SIO2_wt_calibred': x_test, 'ALL_Alkaline_wt_calibred': y_test})
 
 
 # Call the Compare function
 target_probabilities = Compare(df_mugearite, df_test,'SIO2_wt_calibred', 'ALL_Alkaline_wt_calibred')
 
 # Plot training data in blue
-plt.scatter(df_mugearite['SIO2_wt_calibred'], df_mugearite['ALL_Alkaline_wt_calibred'], color='blue', label='Training data',edgecolors= None,alpha=0.3)
+plt.scatter(df_mugearite['SIO2_wt_calibred'], df_mugearite['ALL_Alkaline_wt_calibred'], color='blue', label='Training data',edgecolors= None,alpha=0.1)
 
 
 # Plot testing data in red
 plt.scatter(df_test['SIO2_wt_calibred'], df_test['ALL_Alkaline_wt_calibred'], color='red', label='Testing data',edgecolors= None,alpha=target_probabilities)
 
-# # Add probability labels to the test points
-# for i, (x, y) in enumerate(zip(df_test['SIO2_wt_calibred'], df_test['ALL_Alkaline_wt_calibred'],)):
-#     plt.text(x, y, f"{target_probabilities[i]:.2f}", ha='right')
+# Add probability labels to the test points
+for i, (x, y) in enumerate(zip(df_test['SIO2_wt_calibred'], df_test['ALL_Alkaline_wt_calibred'],)):
+    plt.text(x, y, f"{target_probabilities[i]:.2f}", ha='right')
 
 # Add a legend
 plt.legend()
